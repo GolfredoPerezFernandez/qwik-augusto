df2feec6f15cc10c402449c15b9c41e696b3d796
GolfredoPerezFernandez <golfredo.pf@gmail.com>
2024-03-20 09:36:38 -0400

    asd

--- src/components/starter/hero/hero.tsx
+++ src/components/starter/hero/hero.tsx
@@ -5,7 +5,10 @@ import ImgThunder from "../../../media/thunder.png?jsx";
 export default component$(() => {
   return (
     <div class={["container", styles.hero]}>
-     
+     <video class="w-full" controls>
+  <source src="/docs/videos/flowbite.mp4" type="video/mp4"/>
+  Your browser does not support the video tag.
+</video>
     </div>
   );
 });
